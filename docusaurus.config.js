fb3e43346b885b7503a4cbeb2791d397c60e9d90
Ke Jie <chzealot@gmail.com>
2023-08-16 15:17:18 +0800

    feat: 更新域名
    Signed-off-by: Ke Jie <chzealot@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,12 +6,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: '钉钉开发者百科',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://open-dingtalk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/developerpedia/',
